9e8e95229eda40823d59073b5b51b8e3e16d4beb
sulaiman <160137667+sulaiman1224@users.noreply.github.com>
2024-03-22 11:14:06 +0500

    first

---
+++ my.js
@@ -0,0 +1,13 @@
+const getcolor = () => {
+    const randdomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randdomNumber.toString(16);
+    document.body.style.background = randomCode;
+    document.getElementById("color_code").innerText = randomCode
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+getcolor()
