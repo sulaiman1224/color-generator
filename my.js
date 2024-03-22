89a5837c67bdfdd79a2938e1f6c3efbbe95d587a
sulaiman <160137667+sulaiman1224@users.noreply.github.com>
2024-03-22 12:01:55 +0500

    dd

--- my.js
+++ my.js
@@ -2,6 +2,7 @@ const getcolor = () => {
     const randdomNumber = Math.floor(Math.random() * 16777215);
     const randomCode = "#" + randdomNumber.toString(16);
     document.body.style.background = randomCode;
+    document.getElementById("color_code").style.color=randomCode
     document.getElementById("color_code").innerText = randomCode
 }
 
